d811dd1e28e8c8b24273d2e799a6c30ba64562f4
peploleum <peploleum@gmail.com>
2014-08-29 22:22:05 +0200

    learning d3js

--- WebContent/blossom/blossom.js
+++ WebContent/blossom/blossom.js
@@ -38,18 +38,57 @@ angular
 		.controller(
 				'MainCtrl',
 				function($scope) {
-					//a d3js bit here
-					var data = [ 4, 8, 15, 16, 23, 42, 51 ];
+					// a d3js bit here
+					var data = [ 4, 8, 15, 16, 23, 42 ];
 
-					var x = d3.scale.linear().domain([ 0, d3.max(data) ])
-							.range([ 0, 420 ]);
+					// var x = d3.scale.linear().domain([ 0, d3.max(data) ])
+					// .range([ 0, 420 ]);
+					//
+					// d3.select(".chart").selectAll("div").data(data).enter()
+					// .append("div").style("width", function(d) {
+					// return x(d) + "px";
+					// }).text(function(d) {
+					// return d;
+					// });
 
-					d3.select(".chart").selectAll("div").data(data).enter()
-							.append("div").style("width", function(d) {
-								return x(d) + "px";
-							}).text(function(d) {
+					var width = 420, barHeight = 20;
+
+					var xsvg = d3.scale.linear().domain([ 0, d3.max(data) ])
+							.range([ 0, width ]);
+
+					var chart = d3.select(".chartsvg").attr("width", width)
+							.attr("height", barHeight * data.length);
+
+					var bar = chart.selectAll("g").data(data).enter().append(
+							"g").attr("transform", function(d, i) {
+						return "translate(0," + i * barHeight + ")";
+					});
+
+					bar.append("rect").attr("width", xsvg).attr("height",
+							barHeight - 1);
+
+					bar.append("text").attr("x", function(d) {
+						return xsvg(d) - 3;
+					}).attr("y", barHeight / 2).attr("dy", ".35em").text(
+							function(d) {
 								return d;
 							});
+
+					var graph = d3.select(".graphsvg").attr("width", width)
+							.attr("height", width);
+					graph.append("circle").attr("cx", 90).attr("cy", 90).attr(
+							"r", 30).style("fill", "red");
+					graph.append("circle").attr("cx", 180).attr("cy", 90).attr(
+							"r", 30).style("fill", "blue");
+
+					d3.json("data/data.json", function(json) {
+						$scope.message = "wesh wesh canne à pêche";
+						graph.append("circle").attr("cx", 180).attr("cy", 180)
+								.attr("r", 30).style("fill", "yellow");
+						graph.append("text").attr("x", 180).attr("y", 180)
+								.text("text", "testtext").attr("fill", "red");
+					})
+
 				})
 
 		.controller('AboutCtrl', function($scope) {
@@ -64,6 +103,8 @@ angular
 				'SearchCtrl',
 				function($scope, $http, $templateCache) {
 					$scope.results = [];
+					$scope.solrerror = false;
+					$scope.solrsuccess = true;
 					$scope.submit = function() {
 						$http(
 								{
@@ -74,16 +115,18 @@ angular
 								.success(function(data, status) {
 									$scope.numfound = data.response.numFound;
 									$scope.solrresults = data;
+									$scope.solrerror = false;
+									$scope.solrsuccess = true;
 								})
 								.error(function(data, status) {
 									$scope.numfound = 0;
-									$scope.message = "fail"
+									$scope.solrerror = true;
+									$scope.solrsuccess = false;
 								})
 								.then(
 										function() {
 											$scope.results = [];
 											var localResults = $scope.solrresults.response.docs;
-											$scope.message = localResults[0].id;
 											localResults
 													.forEach(function(element) {
 														$scope.results
