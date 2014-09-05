438fc8fa1ba284472e0a282edd2ca334aa5570e7
Peplo Leum <peploleum@gmail.com>
2014-09-05 22:32:50 +0200

    painfully mavenizing the project and keeping it a web dynamic project running locally + testing clean compile install and deploy in tomcat

--- src/servlets/BlossomStatComputing.java
+++ src/servlets/BlossomStatComputing.java
@@ -1,12 +1,15 @@
 package servlets;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -55,19 +58,25 @@ public class BlossomStatComputing extends HttpServlet {
 			final HttpServletResponse response) throws ServletException,
 			IOException {
 		System.out.println("DOPOST");
-		final BufferedReader inputStream = request.getReader();
-		final StringBuilder sb = new StringBuilder();
-		final char[] buffer = new char[128];
-		int bytesRead = -1;
-		while ((bytesRead = inputStream.read(buffer)) > 0) {
-			sb.append(buffer, 0, bytesRead);
+		// final BufferedReader inputStream = request.getReader();
+		// final StringBuilder sb = new StringBuilder();
+		// final char[] buffer = new char[128];
+		// int bytesRead = -1;
+		// while ((bytesRead = inputStream.read(buffer)) > 0) {
+		// sb.append(buffer, 0, bytesRead);
+		// }
+		// final String string = sb.toString();
+		// final String msg = "received " + string;
+		// logger.fine(msg);
+		// System.out.println(msg);
+		JsonReader createReader = Json.createReader(request.getReader());
+		JsonArray jsonArray = createReader.readArray();
+		for (JsonValue jsonValue : jsonArray) {
+			System.out.println(jsonValue.toString());
 		}
-		final String string = sb.toString();
-		final String msg = "received " + string;
-		logger.fine(msg);
-		System.out.println(msg);
+		// JsonArray jsonObject2 = jsonArray.getJsonArray(1);
 
-		dataSet = string;
+		dataSet = "";
 		final String jsonObject = (dataSet == null || dataSet.isEmpty()) ? "{}"
 				: dataSet;
 		// we flush a simple example down the drain, like a turd
---
+++ src/test/BlossomStatComputingTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BlossomStatComputingTest {
+	@Test
+	public void statComputingTest() {
+		assertTrue(true);
+	}
+}
