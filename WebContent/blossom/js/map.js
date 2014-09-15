ecdb8cedbfe3e5e798fb473e22527b213682d933
peploleum <peploleum@gmail.com>
2014-09-15 23:05:22 +0200

    learning ol3 creating custom controls

--- WebContent/blossom/js/map.js
+++ WebContent/blossom/js/map.js
@@ -1,4 +1,49 @@
 var module = angular.module('blossom.map', [ 'ngRoute' ]);
 
 module.controller('MapCtrl', function($scope) {
+	var exampleNS = {};
+
+	exampleNS.getRendererFromQueryString = function() {
+		var obj = {}, queryString = location.search.slice(1), re = /([^&=]+)=([^&]*)/g, m;
+
+		while (m = re.exec(queryString)) {
+			obj[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+		}
+		if ('renderers' in obj) {
+			return obj['renderers'].split(',');
+		} else if ('renderer' in obj) {
+			return [ obj['renderer'] ];
+		} else {
+			return undefined;
+		}
+	};
+	
+	customControl = function() {
+		console.log("paf");
+		var weshDiv = document.getElementById("wesh");
+		ol.control.Control.call(this, {
+			element : weshDiv,
+		// target: options.target
+		});
+	};
+	ol.inherits(customControl, ol.control.Control);
+
+	var map = new ol.Map({
+		controls : ol.control.defaults(),
+		target : 'map',
+		layers : [ new ol.layer.Tile({
+			source : new ol.source.MapQuest({
+				layer : 'sat'
+			})
+		}) ],
+		renderer : exampleNS.getRendererFromQueryString(),
+		view : new ol.View({
+			center : ol.proj.transform([ 37.41, 8.82 ], 'EPSG:4326', 'EPSG:3857'),
+			zoom : 4
+		})
+	});
+	// new ol.control.FullScreen()
+	map.addControl(new ol.control.FullScreen());
+	map.addControl(new customControl());
+//	map.addControl(new paf());
 })
