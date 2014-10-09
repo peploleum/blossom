2ac7af4ed2e57f9a64e139621d02e9dc63335f02
peploleum <peploleum@gmail.com>
2014-10-10 00:36:41 +0200

    dirty fix to make deployed version OK

--- WebContent/blossom/js/mapsbusinesslayermanager.js
+++ WebContent/blossom/js/mapsbusinesslayermanager.js
@@ -8,7 +8,7 @@ var module = angular.module('blossom.maps.businesslayer', [ 'ngRoute', 'ng' ]);
 // server side.
 module.factory('refresherFactory', [ '$http', '$location', function($http, $location) {
 
-	var urlBase = 'ws://' + $location.host() + ':' + $location.port() + '/Blossom' + '/maps/businesslayerupdates';
+	var urlBase = 'ws://' + $location.host() + ':' + $location.port() + '/' + $location.absUrl().split('/')[3] + '/maps/businesslayerupdates';
 	var refresherFactory = {};
 	var websocket = new WebSocket(urlBase);
 	websocket.onopen = function(event) {
