3c5ddf89885e685ba88863c74968eb5c6e6e6626
peploleum <peploleum@gmail.com>
2014-10-09 20:11:59 +0200

    incons for drawing tools. custom css to draw icons in a span with elegance

--- WebContent/blossom/js/drawingtool.js
+++ WebContent/blossom/js/drawingtool.js
@@ -1,7 +1,4 @@
 var dt = new Object();
-dt.polygon = {
-	"type" : "polygon"
-};
 
 dt.createControls = function(map, linkedElement, button, geomType) {
 	dtControls = function() {
@@ -38,17 +35,10 @@ dt.initDrawingOverlay = function(map, geometryType) {
 	console.log("calling drawing callback");
 	var currentDrawnType = geometryType;
 	dt.removeDrawingOverlay(map);
-	// The features are not added to a regular vector layer/source,
-	// but to a feature overlay which holds a collection of features.
-	// This collection is passed to the modify and also the draw
-	// interaction, so that both can add or modify features.
 	dt.featureOverlay.setMap(map);
 
 	var modify = new ol.interaction.Modify({
 		features : dt.featureOverlay.getFeatures(),
-		// the SHIFT key must be pressed to delete vertices, so
-		// that new vertices can be drawn at the same position
-		// of existing vertices
 		deleteCondition : function(event) {
 			return ol.events.condition.shiftKeyOnly(event) && ol.events.condition.singleClick(event);
 		}
@@ -65,7 +55,6 @@ dt.initDrawingOverlay = function(map, geometryType) {
 		map.addInteraction(dt.draw);
 	}
 
-	// think about what to listen to to change the type
 	addInteraction();
 }
 
