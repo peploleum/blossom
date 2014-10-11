8cef17cd548d2e754b173de336c44507d56c4328
peploleum <peploleum@gmail.com>
2014-10-11 17:32:43 +0200

    struggling with ol3 inheritance

---
+++ WebContent/blossom/js/circleinteraction.js
@@ -0,0 +1,23 @@
+var ci = new Object();
+
+ci.CircleInteraction = function() {
+	// prototype = ol.interaction.Pointer.prototype;
+	ol.interaction.Pointer.call(this);
+};
+// ci.CircleInteraction.prototype = new Object();
+// ci.CircleInteraction.prototype = new Object();
+//ol.inherits(ci.CircleInteraction, ol.interaction.Pointer);
+// ci.CircleInteraction.prototype.handleMapBrowserEvent = function(event) {
+// var map = event.map;
+// console.log("there was a map browser event");
+// if (!map.isDef()) {
+// return true;
+// }
+// var pass = true;
+// if (event.type === ol.MapBrowserEvent.EventType.POINTERMOVE) {
+// pass = this.handlePointerMove_(event);
+// } else if (event.type === ol.MapBrowserEvent.EventType.DBLCLICK) {
+// pass = false;
+// }
+// return true;
+// };
--- WebContent/blossom/js/drawingtool.js
+++ WebContent/blossom/js/drawingtool.js
@@ -12,8 +12,11 @@ dt.createControls = function(map, linkedElement, button, geomType) {
 	ol.inherits(dtControls, ol.control.Control);
 	// adding the drawing control
 	map.addControl(new dtControls());
+
 }
+
 dt.draw = new Object();
+
 dt.featureOverlay = new ol.FeatureOverlay({
 	style : new ol.style.Style({
 		fill : new ol.style.Fill({
@@ -31,31 +34,38 @@ dt.featureOverlay = new ol.FeatureOverlay({
 		})
 	})
 });
+
 dt.initDrawingOverlay = function(map, geometryType) {
 	console.log("calling drawing callback");
 	var currentDrawnType = geometryType;
 	dt.removeDrawingOverlay(map);
 	dt.featureOverlay.setMap(map);
 
-	var modify = new ol.interaction.Modify({
-		features : dt.featureOverlay.getFeatures(),
-		deleteCondition : function(event) {
-			return ol.events.condition.shiftKeyOnly(event) && ol.events.condition.singleClick(event);
-		}
-	});
-	map.addInteraction(modify);
-
-	// global so we can remove it later
-	function addInteraction() {
-		dt.draw = new ol.interaction.Draw({
+	if (geometryType == 'Circle') {
+		console.log("drawing circle")
+	} else {
+		var modify = new ol.interaction.Modify({
 			features : dt.featureOverlay.getFeatures(),
-			type : /** @type {ol.geom.GeometryType} */
-			currentDrawnType
+			deleteCondition : function(event) {
+				return ol.events.condition.shiftKeyOnly(event) && ol.events.condition.singleClick(event);
+			}
 		});
-		map.addInteraction(dt.draw);
+		map.addInteraction(modify);
+
+		// global so we can remove it later
+		function addInteraction() {
+			dt.draw = new ol.interaction.Draw({
+				features : dt.featureOverlay.getFeatures(),
+				type : /** @type {ol.geom.GeometryType} */
+				currentDrawnType
+			});
+			map.addInteraction(dt.draw);
+		}
+
+		addInteraction();
 	}
 
-	addInteraction();
+	// map.addInteraction(ci.CircleInteraction);
 }
 
 dt.removeDrawingOverlay = function(map) {
